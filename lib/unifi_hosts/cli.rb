6c06b280961b2e7b4723b34e0441129e1bd9a2b0
Dave Dribin <dave-vcs@dribin.org>
2020-12-24 14:12:57 -0600

    Add an output file option

--- lib/unifi_hosts/cli.rb
+++ lib/unifi_hosts/cli.rb
@@ -8,6 +8,9 @@ module UnifiHosts
     BASE_COMMAND = File.basename($0)
 
     class_option :verbose, :type => :boolean, :aliases => '-v', :desc => "Enable verbose output"
+    class_option :output, :type => :string, :aliases => '-o', :desc => "Output file"
+    class_option :in_place, :type => :boolean, :aliases => '-o', :desc => "Overwrite input file"
+    class_option :dry_run, :type => :boolean, :aliases => '-n', :desc => "Don't write to output file"
 
     def self.exit_on_failure?
       true
@@ -18,31 +21,35 @@ module UnifiHosts
       hosts_file = read_hosts_file(input_file)
       return 1 if hosts_file.nil?
 
-      hosts_file.headers.each { |h| puts h }
-      entries = hosts_file.entries
-      puts HostEntry.to_s(entries)
+      with_output do |o|
+        hosts_file.headers.each { |h| o.puts h }
+        entries = hosts_file.entries
+        o.puts HostEntry.to_s(entries)
+      end
     end
 
     desc "sort FILE", "Sorts entries in FILE by IP address"
-    method_option :dry_run, :type => :boolean, :aliases => '-n', :desc => "Dry run"
     def sort(input_file)
       hosts_file = read_hosts_file(input_file)
       return 1 if hosts_file.nil?
       
-      hosts_file.headers.each { |h| output h }
-      entries = hosts_file.sort_entries
-      output HostEntry.to_s(entries)
+      with_output do |o|
+        hosts_file.headers.each { |h| o.puts h }
+        entries = hosts_file.sort_entries
+        o.puts HostEntry.to_s(entries)
+      end
     end
 
     desc "dedupe FILE", "Removes dpulicate entries in FILE"
-    method_option :dry_run, :type => :boolean, :aliases => '-n', :desc => "Dry run"
     def dedupe(input_file)
       hosts_file = read_hosts_file(input_file)
       return 1 if hosts_file.nil?
       
-      hosts_file.headers.each { |h| output h }
-      entries = hosts_file.dedupe_entries(&method(:print_dedupe_event))
-      output HostEntry.to_s(entries)
+      with_output do |o|
+        hosts_file.headers.each { |h| o.puts h }
+        entries = hosts_file.dedupe_entries(&method(:print_dedupe_event))
+        o.puts HostEntry.to_s(entries)
+      end
     end
 
     no_commands do
@@ -52,10 +59,10 @@ module UnifiHosts
           hosts_file = File.open(file) do |input|
             HostsFile.read(input)
           end
-      rescue Errno::ENOENT, Errno::EACCES => e
-        $stderr.puts "#{BASE_COMMAND} #{current_command}: #{e.message}"
-      end
-      return hosts_file
+        rescue Errno::ENOENT, Errno::EACCES => e
+          $stderr.puts "#{BASE_COMMAND} #{current_command}: #{e.message}"
+        end
+        return hosts_file
       end
 
       def print_dedupe_event(event, entry)
@@ -63,17 +70,35 @@ module UnifiHosts
 
         verb = case event
         when HostsFile::SKIP
-          "Skip: "
+          "SKIP: "
         when HostsFile::KEEP
-          "Keep: "
+          "KEEP: "
         end
         $stderr.puts "#{verb}#{entry}"
       end
 
-      def output(string)
-        puts string if !@options[:dry_run]
+      # Opens output file and calls the block with the IO handle
+      def with_output
+        result = 0
+        output_file =  options[:output] || "-"
+        if output_file == '-'
+          result = yield(STDOUT)
+        else
+          begin
+            if options[:dry_run]
+              $stderr.puts "Dry run: Not writing to #{output_file}"
+              output_file = '/dev/null' if options[:dry_run]
+            end
+            result = File.open(output_file, "w") { |io|  yield io }
+          rescue Errno::ENOENT, Errno::EACCES => e
+            $stderr.puts "#{BASE_COMMAND} #{current_command}: #{e.message}"
+            result = 1
+          end
+        end
+        return result
       end
 
+      # Override to get current command
       def initialize(args = [], local_options = {}, config = {})
         super(args, local_options, config)
         @current_command = config[:current_command]
@@ -88,7 +113,7 @@ module UnifiHosts
     # Override start to allow commands to return the exit code
     def self.start(given_args = ARGV, config = {})
       exit_code = super given_args, config
-      exit_code = 0 if exit_code.nil?
+      exit_code = 0 if !exit_code.is_a? Integer
       exit(exit_code)
     end
 
